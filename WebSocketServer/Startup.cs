276789cdee06b1cac4d90e90469cef951d10cdbb
Filip Velebit Samodol <fisamodo@vsite.hr>
2020-03-25 23:58:45 +0100

    Added websockets to startup

--- WebSocketServer/Startup.cs
+++ WebSocketServer/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using  System.Net.WebSockets;
+
 
 namespace WebSocketServer
 {
@@ -22,20 +24,7 @@ namespace WebSocketServer
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            app.UseWebSockets();
         }
     }
 }
