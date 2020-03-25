acf5bf861a7acdee9fffabe174328ce85c7de29b
Filip Velebit Samodol <fisamodo@vsite.hr>
2020-03-26 00:04:40 +0100

    added control, checks a websocket, if so creates it

--- WebSocketServer/Startup.cs
+++ WebSocketServer/Startup.cs
@@ -15,16 +15,26 @@ namespace WebSocketServer
 
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        /
         public void ConfigureServices(IServiceCollection services)
         {
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseWebSockets();
+
+            app.Use(async (context, next) => {
+                if(context.WebSockets.IsWebSocketRequest) //checks if it's a websocket, if so we create websocket object with await context.web.....
+                {
+                    WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync(); //async method, may take time
+                    Console.WriteLine("WebSocket Connected");    
+                }
+                else
+                {
+                    await next();
+                }
+            }
         }
     }
 }
