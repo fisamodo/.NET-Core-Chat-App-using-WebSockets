bc06c89e5f741a0027a8f20c3f929dacfb045d4b
Filip Velebit Samodol <fisamodo@vsite.hr>
2020-03-26 14:19:42 +0100

    added a message reciever

--- WebSocketServer/Startup.cs
+++ WebSocketServer/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using  System.Net.WebSockets;
+using System.Threading;
 
 
 namespace WebSocketServer
@@ -24,13 +25,27 @@ namespace WebSocketServer
         {
             app.UseWebSockets(); //middleware, part of pipeline request
 
-            app.Use(async (context, next) => //middleware, part of pipeline request
+            app.Use(async (context, next) => //middleware, part of pipeline request, 2nd request delegate
              {
-                WriteRequestParam(context);
+               // WriteRequestParam(context);
                 if(context.WebSockets.IsWebSocketRequest) //checks if it's a websocket, if so we create websocket object with await context.web.....
                 {
                     WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync(); //async method, may take time, accepts requests
-                    Console.WriteLine("WebSocket Connected");    
+                    Console.WriteLine("WebSocket Connected"); 
+
+                    await ReceiveMessage(webSocket, async(result, Buffer) =>
+                    {
+                        if(result.MessageType == WebSocketMessageType.Text)
+                        {
+                            Console.WriteLine("Message Recieved");
+                            return;
+                        }
+                        else if(result.MessageType == WebSocketMessageType.Close)
+                        {
+                            Console.WriteLine("Recieved Close message");
+                            return;
+                        }
+                    }   );
                 }
                 else
                 {
@@ -39,13 +54,27 @@ namespace WebSocketServer
                 }
             });
 
-            app.Run(async context =>{
-                Console.WriteLine("Hello from the 3rd request delegate."); //runs after 2nd delegate when i go to http://localhost5000
+            app.Run(async context =>{ //3rd request delegate
+                Console.WriteLine("Hello from the 3rd request delegate."); // runs after 2nd delegate when i go to http://localhost5000
                 await context.Response.WriteAsync("Hello from the 3rd request delegate");
             });
+        }
+
+        private async Task ReceiveMessage(WebSocket socket, Action<WebSocketReceiveResult, byte[]> handleMessage)
+        {
+            var buffer = new byte[1024 * 4];
+
+            while(socket.State == WebSocketState.Open)
+            {
+                var result = await socket.ReceiveAsync(buffer: new ArraySegment<byte>(buffer),
+                    cancellationToken: CancellationToken.None);
+                
+                handleMessage(result, buffer);
+            }
+        }
 
         }
-        public void WriteRequestParam(HttpContext context)
+       /* public void WriteRequestParam(HttpContext context)
         {
             Console.WriteLine("Request Method: "+ context.Request.Method);
             Console.WriteLine("Request Protocol: "+ context.Request.Protocol);
@@ -58,6 +87,6 @@ namespace WebSocketServer
                 }
             }
 
-        }
+        }*/
     }
-}
+
