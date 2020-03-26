6966f0b54fe1746e1e2412437cadb522a74ac7d0
Filip Velebit Samodol <fisamodo@vsite.hr>
2020-03-26 13:38:44 +0100

    added 3 request pipelines, works as it should for a change :)

--- WebSocketServer/Startup.cs
+++ WebSocketServer/Startup.cs
@@ -20,22 +20,30 @@ namespace WebSocketServer
         {
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) //request pipeline, google the pic to understand better
         {
-            app.UseWebSockets();
+            app.UseWebSockets(); //middleware, part of pipeline request
 
-            app.Use(async (context, next) =>
+            app.Use(async (context, next) => //middleware, part of pipeline request
              {
                 if(context.WebSockets.IsWebSocketRequest) //checks if it's a websocket, if so we create websocket object with await context.web.....
                 {
-                    WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync(); //async method, may take time
+                    WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync(); //async method, may take time, accepts requests
                     Console.WriteLine("WebSocket Connected");    
                 }
                 else
                 {
-                    await next();
+                    Console.WriteLine("Hello from the 2rd request delegate."); //runs when i go to http://localhost5000
+                    await next(); //creates next request delegate in the pipeline
                 }
             });
+
+            app.Run(async context =>{
+                Console.WriteLine("Hello from the 3rd request delegate."); //runs after 2nd delegate when i go to http://localhost5000
+                await context.Response.WriteAsync("Hello from the 3rd request delegate");
+            });
+
         }
+        public void WriteRequest
     }
 }
